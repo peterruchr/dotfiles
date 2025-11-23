fcbb6e6f13b342fa14a07baf2b098bb98d6208c0
Peter <peterrc@outlook.dk>
2025-11-23 01:10:13 +0100

    start putting things into settings
    update gitignore and remove log files

---
+++ nvim/.config/lua/settings.lua
@@ -0,0 +1,67 @@
+-- Set space as leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Enable nerd font if possible
+vim.g.have_nerd_font = true 
+
+-- Show line numbers and relative line number
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- No need to show the mode (normal, insert etc), this is already handled in the status bar
+vim.o.showmode = false
+
+-- Sync clipboard between OS and Neovim, scheduled since it can take some time.
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
