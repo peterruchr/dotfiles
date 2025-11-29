9c63dc63589d00ca464f5224cedcba7657658caa
Peter <peterrc@outlook.dk>
2025-11-29 11:19:48 +0100

    do rest of components, missing dap config and proper lsp setup for stuff like dotnet.

--- nvim/.config/lua/core/settings.lua
+++ nvim/.config/lua/core/settings.lua
@@ -65,3 +65,22 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+  },
+}
