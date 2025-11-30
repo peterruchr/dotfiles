8475f775a650621c20c6f9e650eafa319250a463
Peter <peterrc@outlook.dk>
2025-11-30 22:48:36 +0100

    fix indent

--- nvim/.config/nvim/lua/core/settings.lua
+++ nvim/.config/nvim/lua/core/settings.lua
@@ -22,9 +22,9 @@ vim.o.breakindent = true
 
 -- setup indentation
 vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
 vim.o.smartindent = true
 
 -- Save undo history
