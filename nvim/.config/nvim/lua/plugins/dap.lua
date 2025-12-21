9e198f8e1ade965484600839d5e4abe34cfcc668
Peter <peterrc@outlook.dk>
2025-12-21 21:10:15 +0100

    configure dap and move dap configs

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -20,7 +20,27 @@ return {
 			"nvim-neotest/nvim-nio",
 		},
 		config = function()
-			require("dapui").setup()
+			local dapui = require("dapui")
+			dapui.setup()
+
+			local dap = require("dap")
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.after.event_output.dapui_config = function(_, body)
+				if body.category == "console" then
+					dapui.eval(body.output)
+				end
+			end
 		end,
 	},
 
