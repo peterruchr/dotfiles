ba0bd7bc7d316b1df44842bb8edde2162263507b
Peter <peterrc@outlook.dk>
2025-12-21 22:43:15 +0100

    configure dap somewhat correctly

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -4,12 +4,59 @@ return {
 		config = function()
 			local dap = require("dap")
 
-			vim.keymap.set("n", "<F5>", dap.restart, { desc = "Debug: Restart" })
-			vim.keymap.set("n", "<F9>", dap.continue, { desc = "Debug: Continue" })
-			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step over" })
-			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step into" })
-			vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step out" })
-			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle breakpoint" })
+			vim.keymap.set("n", "<F1>", dap.restart, { desc = "Debug: Restart" })
+			vim.keymap.set("n", "<F2>", dap.continue, { desc = "Debug: Continue" })
+			vim.keymap.set("n", "<F3>", dap.step_over, { desc = "Debug: Step over" })
+			vim.keymap.set("n", "<F4>", dap.step_into, { desc = "Debug: Step into" })
+			vim.keymap.set("n", "<F5>", dap.step_out, { desc = "Debug: Step out" })
+			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug: Toggle breakpoint" })
+			vim.keymap.set("n", "<leader>dt", function()
+				require("dap").terminate()
+				require("dapui").close()
+			end)
+
+			vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "Error", linehl = "", numhl = "" })
+			vim.fn.sign_define("DapStopped", { text = "→", texthl = "Error", linehl = "DiffAdd", numhl = "" })
+
+			dap.adapters.coreclr = {
+				type = "executable",
+				command = vim.fn.exepath("netcoredbg") or vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
+				args = { "--interpreter=vscode" },
+			}
+
+			dap.configurations.cs = {
+				{
+					type = "coreclr",
+					name = "Launch",
+					request = "launch",
+					program = function()
+						local project_path = vim.fs.root(0, function(name)
+							return name:match("%.csproj$") ~= nil
+						end)
+
+						if not project_path then
+							return vim.notify("Couldn't find the csproj path")
+						end
+
+						return require("dap.utils").pick_file({
+							filter = string.format("Debug/.*/%s", vim.fn.fnamemodify(project_path, ":t:r")),
+							path = string.format("%s/bin", project_path),
+						})
+					end,
+				},
+				{
+					type = "coreclr",
+					name = "Attach to process",
+					request = "attach",
+					processId = function()
+						return require("dap.utils").pick_process({
+							filter = function(p)
+								return p.name:match(".*/Debug/.*") and not p.name:find("vstest.console.dll")
+							end,
+						})
+					end,
+				},
+			}
 		end,
 	},
 
@@ -30,10 +77,10 @@ return {
 			dap.listeners.before.launch.dapui_config = function()
 				dapui.open()
 			end
-			dap.listeners.before.event_terminated.dapui_config = function()
+			dap.listeners.after.event_terminated.dapui_config = function()
 				dapui.close()
 			end
-			dap.listeners.before.event_exited.dapui_config = function()
+			dap.listeners.after.event_exited.dapui_config = function()
 				dapui.close()
 			end
 			dap.listeners.after.event_output.dapui_config = function(_, body)
@@ -62,51 +109,6 @@ return {
 						require("mason-nvim-dap").default_setup(config)
 					end,
 				},
-				-- DOTNET setup
-				coreclr = function(config)
-					config.adapters = {
-						type = "executable",
-						command = vim.fn.exepath("netcoredbg"),
-						args = { "--interpreter=vscode" },
-					}
-
-					config.configurations = {
-						{
-							type = "coreclr",
-							name = "Launch - NET core",
-							request = "launch",
-							program = function()
-								local project_path = vim.fs.root(0, function(name)
-									return name:match("%.csproj$") ~= nil
-								end)
-
-								if not project_path then
-									return vim.notify("Couldn't find the csproj path")
-								end
-
-								return require("dap.utils").pick_file({
-									filter = string.format("Debug/.*/%s", vim.fn.fnamemodify(project_path, ":t:r")),
-									path = string.format("%s/bin", project_path),
-								})
-							end,
-						},
-						{
-							type = "coreclr",
-							name = "Attach - NET core",
-							request = "attach",
-							processId = function()
-								return require("dap.utils").pick_process({
-									filter = function(proc)
-										---@diagnostic disable-next-line: return-type-mismatch
-										return proc.name:match(".*/Debug/.*")
-											and not proc.name:find("vstest.console.dll")
-									end,
-								})
-							end,
-						},
-					}
-					require("mason-nvim-dap").default_setup(config)
-				end,
 			})
 		end,
 	},
