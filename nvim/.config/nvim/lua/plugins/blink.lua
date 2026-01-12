8a88e4f158054767d5e322c2549a1e8efa233d33
Peter <peterrc@outlook.dk>
2026-01-12 17:09:04 +0100

    add friendly snippets and allow to delete buffers through the buffer picker

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -20,12 +20,14 @@ return { -- Autocompletion
 				-- `friendly-snippets` contains a variety of premade snippets.
 				--    See the README about individual language/framework/plugin snippets:
 				--    https://github.com/rafamadriz/friendly-snippets
-				-- {
-				--   'rafamadriz/friendly-snippets',
-				--   config = function()
-				--     require('luasnip.loaders.from_vscode').lazy_load()
-				--   end,
-				-- },
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+
+						require("luasnip").filetype_extend("cs", { "csharpdoc" })
+					end,
+				},
 			},
 			opts = {},
 		},
