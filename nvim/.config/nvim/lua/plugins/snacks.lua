ed60d09b56f4abf97b51adae5554c95f6f7a90a4
Peter <peterrc@outlook.dk>
2026-03-01 01:55:44 +0100

    add snacks and mini plugins

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = true },
+		input = { enabled = true },
+		statuscolumn = { enabled = true },
+		notifier = { enabled = true },
+		scroll = { enabled = true },
+	},
+}
